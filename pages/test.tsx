9c9745469ab9f901672755ff2657fc9fed9a79e7
tetuyoko <tyokoyama53@gmail.com>
2022-05-27 15:12:35 +0900

    feat: Use vercel deploy

---
+++ pages/test.tsx
@@ -0,0 +1,46 @@
+import type { NextPage } from 'next'
+import { GetServerSideProps } from "next";
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+type Props = {
+  email?: string;
+  linked?: boolean;
+}
+
+const TestPage: NextPage = (props:Props) => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Test Page</title>
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+        <p className={styles.description}>
+          email: `{props?.email}`
+          linked: `{props?.linked?.toString()}`
+        </p>
+
+        <p className={styles.description}>
+          Get started by editing{' '}
+          <code className={styles.code}>pages/test.tsx</code>
+        </p>
+      </main>
+    </div>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const res = await fetch('http://localhost:3000/api/foobar')
+  const data = await res.json()
+  const props: Props = {
+    email: data.email,
+    linked: data.linked,
+  }
+  return { props }
+}
+
+export default TestPage
