b3e7e769f0a96cafc0bf670d76bcfb573224a7ff
tetuyoko <tyokoyama53@gmail.com>
2022-05-28 12:13:47 +0900

    feat: Add JobQueue

--- pages/api/queues/reminder.js
+++
@@ -1,8 +0,0 @@
-import { Queue } from "quirrel/next";
-
-export default Queue(
-  "api/queues/reminder", // the route it's reachable under
-  async (recipient) => {
-    console.log(`Sending an E-Mail to ${recipient}`);
-  }
-);
--- pages/api/setupReminder.ts
+++ pages/api/setupReminder.ts
@@ -1,16 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import reminderQueue from "./queues/reminder";
 
 type Data = {
   body: string;
 };
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
   const email = req.body;
   const body = `I'll setup the reminder for ${email}.`;
   console.log(body);
+  await reminderQueue.enqueue(email, {
+    id: email,
+  });
   res.status(200).json({ body });
 }
