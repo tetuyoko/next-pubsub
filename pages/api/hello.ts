721fb46bfd863b6d424002dc37ca1d5cd2ee299d
tetuyoko <tyokoyama53@gmail.com>
2022-06-01 17:37:52 +0900

    fix: Fixed type

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   console.log("hgell");
   res.status(200).json({ name: "John Doe" });
